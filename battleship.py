3aefd74709a0835ce380c7b5ca70dd1187b84723
Grecia Meneses <gmeneses.irais@gmail.com>
2025-09-06 07:20:22 -0600

    changes

--- battleship.py
+++ battleship.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import time
 import os
+import re
 import random 
 from sys import exit
 
@@ -13,13 +14,28 @@ FPS = 60
 font_path = "AI-battlefield/others/upheavtt.ttf"
 COLOR_inner = '#E5E5E5' # light white
 COLOR_back = "#EEEEEE"
-COLOR_opp = "#DFD3D2"
-COLOR_lining = "#4849B1"
+COLOR_opp = "#DFD2D7"
+COLOR_lining = "#90a7c6"#296270" #4849B1"
 COLOR_hover = "#B8C4CE"
+COLOR_font = "#4f7d92" #133844"
+COLOR_main = "#a9d6e4" # #ADCCDA # "#C2DCEB"
 CELL_SIZE = 40 # px
+
+class Ships:
+    def __init__(self, img, pos, length=1):
+        self.length = length
+        self.image = pygame.transform.scale(img, (CELL_SIZE, CELL_SIZE * length))
+        self.rect = self.image.get_rect(topleft=pos)
+        self.dragging = False
+        self.offset_x = 0
+        self.offset_y = 0
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
 
 # for sprite and image handling
 imgs = {}   # dictionary for loaded images
+ships = []
 images_path = [
     ("playBtn", "AI-battlefield\sprites\PlayButton.png", "A"),
     ("background", "AI-battlefield\sprites\Background.png", "C"),
@@ -36,7 +52,7 @@ def create_board(size, def_value=' '): # ~ for water
 
 player_board = create_board(BOARD_SIZE)
 
-def ingame_board(surface, board, leftRight, colorgrid):
+def ingame_board(surface, board, leftRight, colorgrid, font):
     if leftRight == 'L':
         offset_x = (SCREEN_WIDTH - 950) // 2   # 500: CELLS TOP * PIXELS
         offset_y = (SCREEN_HEIGHT - 450) // 2  
@@ -51,6 +67,20 @@ def ingame_board(surface, board, leftRight, colorgrid):
                 pygame.draw.rect(surface, colorgrid, rectangle, border_radius = 6)
                 pygame.draw.rect(surface, COLOR_lining, rectangle, 1,  border_radius = 6)
 
+    rows = len(board)
+    cols = len(board[0])
+
+    for i in range(rows):
+        num_rows = font.render(chr(ord('@')+(i+1)), True, COLOR_font)
+        x = offset_x - 25
+        y = offset_y + i*CELL_SIZE + CELL_SIZE//2 - num_rows.get_height()//2
+        surface.blit(num_rows, (x, y))
+    for j in range(cols):
+        num_cols = font.render(str(j+1), True, COLOR_font)
+        x = offset_x + j*CELL_SIZE + CELL_SIZE//2 - num_cols.get_width()//2
+        y = offset_y - 25
+        surface.blit(num_cols, (x, y))
+    
     return offset_x, offset_y
 
 def get_hover_cell(x, y, board, offset_x, offset_y):
@@ -80,25 +110,32 @@ def draw_hover(surface, hover_cell, cell_size=CELL_SIZE, color=COLOR_hover, offs
     y = offset_y + row * cell_size
     surface.blit(hover_surf, (x, y))
 
+
 def background_blocks(width, height, screen):
     rect = pygame.Rect(0,0,width, height)
     rect.center = (screen.get_width() // 2, screen.get_height() // 2)
-    pygame.draw.rect(screen, pygame.Color(COLOR_back), rect, border_radius=6)
+    pygame.draw.rect(screen, pygame.Color(COLOR_back), rect, border_radius=18)
 
 
 def main():
     pygame.init()
 
     # Setting the window
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # pygame.FULLSCREEN
     pygame.display.set_caption("Battleship")
-    font = pygame.font.Font(font_path, size=12)
+    font = pygame.font.Font(font_path, size=18)
 
     # Image loading
     for name, path, CA in images_path: 
         try:
             if CA == "C":
                 imgs[name] = pygame.image.load(path).convert()
+                match = re.match(r'[a-z]*[A-Z]*[a-z]*([0-9]+)$', name)  # adjust logic as needed
+                if match:
+                    length = int(match.group(1))  # extract number at the end
+                    ship = Ships(imgs[name], (50, SCREEN_HEIGHT-100), length)
+                    ships.append(ship)
+                    start_x += CELL_SIZE + 20
             elif CA == "A":
                 imgs[name] = pygame.image.load(path).convert_alpha()
         except pygame.error as e:
@@ -119,15 +156,22 @@ def main():
     running = True
     while running:
         for event in pygame.event.get():
+            # EVENT HANDLING
             if event.type == pygame.QUIT:
                 running = False
+            
+            # DRAGGING SHIPS
+
+
+            # DROP PIECES
+
 
             # loads background and mouse
             screen.blit(imgs["background"], (0,0))
 
             background_blocks(1050, 550, screen)
-            offset_px1, offset_py1 = ingame_board(screen, player_board, 'L', "#C2DCEB")
-            offset_px2, offset_py2 = ingame_board(screen, player_board, 'R', COLOR_opp)
+            offset_px1, offset_py1 = ingame_board(screen, player_board, 'L', COLOR_main, font)
+            offset_px2, offset_py2 = ingame_board(screen, player_board, 'R', COLOR_opp, font)
             
             x, y = pygame.mouse.get_pos()
             hover_1 = get_hover_cell(x, y, player_board, offset_px1, offset_py1)
@@ -139,6 +183,9 @@ def main():
             cy = y-imgs["mouse"].get_height() // 2
             screen.blit(imgs["mouse"], (cx,cy))
     
+            for ship in ships:
+                ship.draw(screen)
+
             pygame.display.flip()
 
 
