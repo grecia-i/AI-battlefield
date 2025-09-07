bb34dfe4284671c7c86f26819cede0e539348bf3
Grecia Meneses <gmeneses.irais@gmail.com>
2025-09-06 20:15:01 -0600

    settings and ctes

---
+++ settings.py
@@ -0,0 +1,25 @@
+import pygame
+
+# pygame setup constants
+BOARD_SIZE = 10
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 700
+FPS = 60
+BTN_WIDTH = 400
+BTN_HEIGHT = 80
+font_path = "AI-battlefield/others/upheavtt.ttf"
+#font_path = "Battlefield/AI-battlefield/others/upheavtt.ttf"
+COLOR_inner = '#E5E5E5' # light white
+COLOR_back = "#EEEEEE"
+COLOR_opp = "#DFD2D7"
+COLOR_lining = "#90a7c6"#296270" #4849B1"
+COLOR_hover = "#B8C4CECF"
+COLOR_font = "#4f7d92" #133844"
+COLOR_main = "#a9d6e4" # #ADCCDA # "#C2DCEB"
+COLOR_btn = "#5096a7"
+COLOR_btn_font = "#c6e1f3"
+CELL_SIZE = 30 # px
+AI_PLAYER = 0
+AI_AI = 1
+
+imgs = {}
